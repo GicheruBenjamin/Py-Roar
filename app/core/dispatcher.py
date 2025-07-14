bddf2780211e0d821b7f3ceffb77dd1a8b5cfda7
GicheruBenjamin <benjaminnjuguna77@gmail.com>
2025-07-14 21:27:06 +0300

    feature : Session one of core == Types..

---
+++ app/core/dispatcher.py
@@ -0,0 +1,10 @@
+
+
+# app/core/dispatcher.py
+from .types import Dispatchertype
+
+async def dispatcher(d: Dispatchertype):
+    """
+    Process all socket dispatches in the app
+    """
+    pass
